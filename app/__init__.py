ec291cb9c4f350842fed973f5e68e0da2d920c3b
caochangqing <a18615768078@163.com>
2023-12-15 19:12:16 +0800

    Add new changes

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,18 @@
 from flask import Flask
 from flask_session import Session
 from .models import mongo
+from flask_pymongo import PyMongo
 
 def create_app():
     app = Flask(__name__)
     app.config["MONGO_URI"] = "mongodb://49.232.165.6:3389/topasteit"
-    app.config["SECRET_KEY"] = "supersecretkey" # 随意选择一个密钥
-    app.config["SESSION_TYPE"] = "filesystem"
+    app.config["MONGO_CONNECT_TIMEOUT_MS"] = 30000
+    app.config["MONGO_SOCKET_TIMEOUT_MS"] = 30000
+    # app.config["SECRET_KEY"] = "supersecretkey" # 随意选择一个密钥
+    # app.config["SESSION_TYPE"] = "filesystem"
 
-    mongo.init_app(app)
+    mongo = PyMongo(app, connect=True)
+    # 移除mongo.init_app(app)这一行
     Session(app)
 
     from . import views
