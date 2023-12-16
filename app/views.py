6065f3aac308ba85514dcd80e747fb1bd70a1330
caochangqing <a18615768078@163.com>
2023-12-16 21:30:57 +0800

    Add new changes

--- app/views.py
+++ app/views.py
@@ -7,6 +7,14 @@ import pytesseract
 import uuid
 from datetime import timedelta
 from random import random
+from aip import AipOcr
+
+# 请替换为您的百度 OCR API 密钥
+APP_ID = '45118422'
+API_KEY = 'iFZhoC6GKbbv7qEOnLr3gnfl'
+SECRET_KEY = 'ye30G6Tm0eYKR8gy48ZbeItYH10kDuQK'
+
+client = AipOcr(APP_ID, API_KEY, SECRET_KEY)
 
 bp = Blueprint('main', __name__)
 
@@ -26,6 +34,25 @@ def save():
 @bp.route('/ocr', methods=['POST'])
 def ocr():
     image_data = request.form['image'].split(',')[1]
-    image = Image.open(BytesIO(base64.b64decode(image_data)))
-    text = pytesseract.image_to_string(image)
+    image = BytesIO(base64.b64decode(image_data))
+
+    # 使用百度 OCR 对图像进行识别
+    options = {
+        'language_type': 'CHN_ENG',
+    }
+    response = client.basicGeneral(image.read(), options)
+
+    # 提取识别的文本
+    if 'words_result' in response:
+        text = '\n'.join([item['words'] for item in response['words_result']])
+    else:
+        text = ''
+
     return jsonify(text=text)
+
+# @bp.route('/ocr', methods=['POST'])
+# def ocr():
+#     image_data = request.form['image'].split(',')[1]
+#     image = Image.open(BytesIO(base64.b64decode(image_data)))
+#     text = pytesseract.image_to_string(image)
+#     return jsonify(text=text)
